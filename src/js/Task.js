8997a0704567db0294de3811e46d288e6310e24f
unker <kfi.unknown@gmail.com>
2023-08-21 23:24:00 +0300

    add/remove tasks

--- src/js/Task.js
+++ src/js/Task.js
@@ -3,9 +3,10 @@ export default class Task {
     this.container = container;
 
     const tasks = document.createElement('div');
-    tasks.classList.add('list_tasks');
+    tasks.classList.add('container-task');
     container.appendChild(tasks);
 
+    // содержимое контейнера
     const header = document.createElement('div');
     header.classList.add('tasks_header');
     tasks.appendChild(header);
@@ -25,16 +26,84 @@ export default class Task {
     adderCard.textContent = '+ Add another card';
     tasks.appendChild(adderCard);
 
-    adderCard.addEventListener('click', (event) => this.onAddCardClick(event));
+    const task_list = document.createElement('div');
+    task_list.classList.add('task_list');
+    tasks.appendChild(task_list);
+
+    adderCard.addEventListener('click', (e) => this.onAddCardClick(e));
+
+    task_list.addEventListener('mousedown', (e) => this.onMouseDownTask(e)); 
 
     this.tasks = tasks;
+    this.task_list = task_list;
+    this.adderCard = adderCard;
   }
 
-  onAddCardClick(event) {
-    console.log('add card');
+  onMouseDownTask(e) {
+    e.preventDefault();
+
+    this.draggingEl = e.target;
+    // если элемент не таск, то игнорируем событие
+    if (!this.draggingEl.classList.contains('task_card')) {
+      return;
+    }
+    this.draggingEl.classList.add('grabbed');
+
+    document.documentElement.addEventListener('mouseup', this.onMouseUp(e));
+    document.documentElement.addEventListener('mouseover', this.onMouseOver(e));
   }
 
-  addCard(text) {
-    // this.tasks.appendChild
+  onMouseUp() {
+    console.log('up');
+  }
+
+  onMouseOver() {
+    console.log('over');
+  }
+
+  onAddCardClick(e) {
+    this.#addCard();
+  }
+
+  #addCard() {
+    const card = document.createElement("div");
+    card.classList.add("task_card");
+    
+    const closeButton = document.createElement("span");
+    closeButton.classList.add("close_button");
+    closeButton.innerText = '×';
+
+    // Удаляем блок задачи при клике на крестик
+    closeButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      card.remove(); 
+    });
+    
+    card.appendChild(closeButton);
+
+    // блок ввода текста
+    const card_text = document.createElement("div");
+    card_text.classList.add("content_card");
+    card_text.innerText = 'Нажмите, чтобы начать редактирование';
+    card.appendChild(card_text);
+
+    const card_text_edit = document.createElement("div");
+    card_text_edit.classList.add("card_text_edit");
+    card_text_edit.innerText = '\u270E';
+    card.appendChild(card_text_edit);
+
+    card_text_edit.addEventListener('click', () => {
+      // если это первое редактирование, то удалим предыдущий текст
+      console.log(card_text.getAttribute('contenteditable'))
+      if (card_text.getAttribute('contenteditable') === null) {
+        card_text.textContent = '';
+      }
+      card_text.setAttribute('contenteditable', 'true');
+      card_text.focus();
+    });
+
+    // this.task_list.appendChild(card);
+    this.tasks.insertBefore(card, this.adderCard);
+    
   }
 }
